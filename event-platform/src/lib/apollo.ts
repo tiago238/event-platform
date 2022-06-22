86b93d59ed7ba5c557feb048f9790f195f5a752b
tiago238 <tiago_martins238@hotmail.com>
2022-06-22 10:08:20 -0300

    Created Sidebar and Header

--- event-platform/src/lib/apollo.ts
+++ event-platform/src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'fuhuhuhuhuh',
+    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4ofc43601eo01z4cwwg4rk7/master',
     cache: new InMemoryCache()
 })
